50e8c8bc11d5f51de0860825c0c146dd674c0160
nkwsqyyzx <nk.wangshuangquan@gmail.com>
2014-03-10 17:34:21 +0100

    将取不到的文件重定向

--- betpick/urls.py
+++ betpick/urls.py
@@ -1,15 +1,13 @@
 from django.conf.urls import patterns, include, url
-
+from django.views.generic import RedirectView
 from django.contrib import admin
 from betpick.views import odds,home
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'betpick.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     #url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'betpick.views.home', name='home'),
     url(r'^(\d+)/$', odds),
+    url(r'^amline/amcharts_key.txt$', RedirectView.as_view(url='/static/amline/amcharts_key.txt')),
 )

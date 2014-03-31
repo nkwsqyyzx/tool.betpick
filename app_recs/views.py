40ae765e408e2cbd3761e5b03f59d43665294429
nkwsqyyzx <nk.wangshuangquan@gmail.com>
2014-03-31 07:59:48 +0100

    改为更简洁的实现

--- app_recs/views.py
+++ app_recs/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render_to_response
 from django.template import Template, Context
 from django.template.loader import get_template
 
@@ -8,17 +8,9 @@ from app_recs.models import Recommend
 
 # Create your views here.
 def home(request):
-    p = Recommend.objects.filter(Result='-').order_by('-Time2')
-    a = [m for m in p]
-    c = Context({'Recommends': a})
-    t = get_template('app_recs/templates/app_recs/Recommends.html')
-    html = t.render(c)
-    return HttpResponse(html)
+    Recommends = Recommend.objects.filter(Result='-').order_by('-Time2')
+    return render_to_response('app_recs/templates/app_recs/Recommends.html',locals())
 
 def latest(request):
-    p = Recommend.objects.order_by('-Time2')[:100]
-    a = [m for m in p]
-    c = Context({'Recommends': a})
-    t = get_template('app_recs/templates/app_recs/Recommends.html')
-    html = t.render(c)
-    return HttpResponse(html)
+    Recommends = Recommend.objects.order_by('-Time2')[:100]
+    return render_to_response('app_recs/templates/app_recs/Recommends.html',locals())

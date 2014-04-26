c12d8750c486d613543d53c917a6dc24d269b7e7
nkwsqyyzx <nk.wangshuangquan@gmail.com>
2014-04-26 15:40:26 +0100

    fix bug

--- app_stat/tool/StatProvider.py
+++ app_stat/tool/StatProvider.py
@@ -14,6 +14,10 @@ def GetMatchesLink(leagueId):
     html = cache.getContent(url,timeout=timeout)
     soup = BeautifulSoup(html)
     sel_Match = soup.find('select',{'id':'sel_Match'})
+    if "var espn = 'true';" in html:
+        espn = 'true'
+    else:
+        espn = 'false'
     matches = []
     if sel_Match:
         for o in sel_Match.findAll('option'):
@@ -27,7 +31,6 @@ def GetMatchesLink(leagueId):
                 awayMC = m[5]
                 homeTC = m[4]
                 awayTC = m[6]
-                espn = 'true'
                 hora = 'both'
                 uri = "http://www.simplesoccerstats.com/consoles/metrics/matches.php?home=" + homeTeam + "&away=" + awayTeam + "&HMC=" + homeMC + "&HTC=" + homeTC + "&AMC=" + awayMC + "&ATC=" + awayTC + "&hora=" + hora + "&espn=" + espn
                 matches.append((homeTeam,awayTeam,uri))
@@ -66,10 +69,10 @@ def GetMatchesByLink(link):
     return (idshome,idsaway)
 
 import time
-def GetStatics(matchid):
+def GetStatics(matchid,flag):
     # 获取比赛统计数据
     # (时间,对阵双方,比分,半场比分,射门,射正,角球,首先达到3角球,首先达到5角球,首先达到7角球,犯规,黄牌,红牌,越位,控球率)
-    url = 'http://www.simplesoccerstats.com/consoles/metrics/match.php?id={0}&espn=true'.format(matchid)
+    url = 'http://www.simplesoccerstats.com/consoles/metrics/match.php?id={0}&espn={1}'.format(matchid,('true' if flag else 'false'))
     # 数据缓存4个月
     html = cache.getContent(url,timeout=4*30*24*60*60)
     p = html.split('</br>')[1:-1]
@@ -80,14 +83,25 @@ def GetStatics(matchid):
     p4 = p[4].split(' Shots ')
     p5 = p[5].split(' Shots on Target ')
     p6 = p[6].split(' Corners ')
-    p7 = p[7].split(' Race to 3 ')
-    p8 = p[8].split(' Race to 5 ')
-    p9 = p[9].split(' Race to 7 ')
-    p10 = p[10].split(' Fouls ')
-    p11 = p[11].split(' Yellow Cards ')
-    p12 = p[12].split(' Red Cards ')
-    p13 = p[13].split(' Offsides ')
-    p14 = p[14].split(' Possession ')
+    if ' Race to 3 ' in html:
+        p7 = p[7].split(' Race to 3 ')
+        p8 = p[8].split(' Race to 5 ')
+        p9 = p[9].split(' Race to 7 ')
+        p10 = p[10].split(' Fouls ')
+        p11 = p[11].split(' Yellow Cards ')
+        p12 = p[12].split(' Red Cards ')
+        p13 = p[13].split(' Offsides ')
+        p14 = p[14].split(' Possession ')
+    else:
+        p7 = ['-','-']
+        p8 = ['-','-']
+        p9 = ['-','-']
+        p10 = p[7].split(' Fouls ')
+        p11 = p[8].split(' Yellow Cards ')
+        p12 = p[9].split(' Red Cards ')
+        p13 = ['-','-']
+        p14 = ['-','-']
+
 
     home = Stat()
     home.team = p1[0]
@@ -159,9 +173,10 @@ class Stat:
 
 
 if __name__ == "__main__":
-    home,away,link = GetMatchesLink(leagueId=0)[1]
-    homeMatches,awayMatches = GetMatchesByLink(link=link)
-    for matchid in homeMatches:
-        t,h,a = GetStatics(matchid=matchid)
-        print(t,h,a)
-        break;
+    # home,away,link = GetMatchesLink(leagueId=0)[1]
+    # homeMatches,awayMatches = GetMatchesByLink(link=link)
+    # for matchid in homeMatches:
+    #     t,h,a = GetStatics(matchid=matchid)
+    #     print(t,h,a)
+    #     break;
+    t,h,a = GetStatics(matchid='862',flag=False)
